dd6e916813e841684b3194b28835551207f46cc8
zani22ef <zanifitriyanto@gmail.com>
2024-06-10 01:08:47 +0700

    master: Melengkapi nav bottom secera keseluruhan

--- Program/app/src/main/java/com/uti/turtleguard/dashboard/Bottom_Nav.kt
+++ Program/app/src/main/java/com/uti/turtleguard/dashboard/Bottom_Nav.kt
@@ -1,15 +1,11 @@
-package com.uti.turtleguard.form
+package com.uti.turtleguard.dashboard
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.uti.turtleguard.R
-import com.uti.turtleguard.config.Lite
-import com.uti.turtleguard.dashboard.Bottom_Nav
-import com.uti.turtleguard.databinding.FragmentLoginFormBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFormFragment.newInstance] factory method to
+ * Use the [Bottom_Nav.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFormFragment : Fragment() {
+class Bottom_Nav : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private lateinit var binding: FragmentLoginFormBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,32 +35,8 @@ class LoginFormFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_login_form, container, false)
-//        deklarasi sekaligus definisi variabel binding
-        val binding = FragmentLoginFormBinding.inflate(inflater, container, false)
-        val lite = Lite(requireContext())
-
-
-        binding.btnSignin.setOnClickListener() {
-            val username = binding.inputUsername.text.toString()
-            val pass = binding.inputPassword.text.toString()
-            val loginSukses = lite.login(username, pass)
-            if (loginSukses) {
-                Toast.makeText(requireContext(), "Login Berhasil", Toast.LENGTH_SHORT).show()
-
-            } else {
-                Toast.makeText(requireContext(), "Username/Password Salah", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-        binding.txtSignup.setOnClickListener() {
-            requireActivity().supportFragmentManager.beginTransaction().addToBackStack(null)
-                .replace(R.id.frmMain, SignupFormFragment()).commit()
-        }
-
-        return binding.root
+        return inflater.inflate(R.layout.fragment_bottom__nav, container, false)
     }
-
 
     companion object {
         /**
@@ -75,12 +45,12 @@ class LoginFormFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFormFragment.
+         * @return A new instance of fragment Bottom_Nav.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginFormFragment().apply {
+            Bottom_Nav().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
